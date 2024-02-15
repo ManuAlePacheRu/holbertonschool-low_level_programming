eac74f6a6fa54bd07ce970fc1e8a1e5907510e4f
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-15 12:23:45 -0300

    C Project Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: 'The program should show a strin saying \"Programming is like building a multilingual puzzle using 'puts' function
- * Return: Always \0 
+ * Return: Always \0
  */
 
 int main(void)
