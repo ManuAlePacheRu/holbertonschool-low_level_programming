025853e6ccc8629822b1ba52974d9ff70c9b26cf
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-15 12:21:49 -0300

    C Project Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program should show a strin saying \"Programming is like building a multilingual puzzle using 'puts' function
+ * Return: Always \0 
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
