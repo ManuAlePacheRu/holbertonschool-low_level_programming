c3747253f29064568b9b51faf6a14200172e832c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-19 06:15:38 -0800

    C Project Part 2 Task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,13 @@ int main(void)
 	{
 		char l;
 
+
 			for (l = 'a'; l <= 'z'; l++)
 			{
 				putchar(l);
-
+			}
 				for (l = 'A'; l <= 'Z'; l++)
-
+			{
 				putchar(l);
 			}
 				putchar('\n');
