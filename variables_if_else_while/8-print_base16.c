15855742e388adcf308f1851059d81d885bfbb4b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-20 06:39:05 -0800

    C Project Part 2 Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	int l;
+
+			for (l = 0; l <= 10; l++)
+
+			{
+
+				putchar(l = 0);
+
+			}
+
+			for(l = 0; l <= 6; l++)
+
+			{
+
+				putchar(l = 'a');
+
+			}
+
+
+			putchar('\n');
+
+	return (0);
+
+}
