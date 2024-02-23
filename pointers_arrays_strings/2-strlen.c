2fbb2fb63c79a3b008dc8c60b69d6cfe3528767e
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-23 14:13:43 +0000

    Pointers, arrays and strings Task 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen - take string drection
+ * @s: take the string
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+
+{
+
+	int i = 0;
+
+
+	while (s[i] != '\0')
+	{
+
+		i++;
+
+	}
+
+	return (i);
+
+}
