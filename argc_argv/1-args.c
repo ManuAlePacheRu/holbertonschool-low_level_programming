af20a4a9c14f38ac15ed22327dea6456d9270467
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-28 16:38:41 +0000

    argc and argv Project 2 Task 0

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - put the argc and argv arguments
+ * @argc: represent the number of command line arguments
+ * @argv: list the command line arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+
+{
+	(void)argv;
+
+	printf("argc = %d", argc);
+
+	return (0);
+}
