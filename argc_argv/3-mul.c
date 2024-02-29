1cb81c0a87dde3d43e5b88beb48b535461a2f334
ManuAlePacheRu <manualepacheru@gmail.com>
2024-02-29 13:42:54 +0000

    argc and argv Project Task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,8 +13,8 @@ int main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		num1 = argv[1];
-		num2 = argv[2];
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
 		res = num1 * num2;
 		printf("%d\n", res);
 		return (0);
