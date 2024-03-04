6d3cb5dc8fe25deb81fef4062af82f12de138214
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-04 21:34:33 +0000

    malloc-free Project Task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int charspace;
+	char *p
+
+	for (charspace = 0; str[charspace] != '\0'; charspace++)
+	{
+	}
+	p = malloc(sizeof(char)*(charspace + 1);
+	
+			for (charspace = 0; charspace <= str; charsapce++)
