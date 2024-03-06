7bbeb03efb97c2cff5e5531393f41f58a128c430
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-06 16:32:38 +0000

    Function Nested Loops Project Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - compute absolute value of a int
+ *@num: this will be the computed integer
+ *Return: the absolute value of the int
+ */
+
+int _abs(int num)
+{
+
+	if (num < 0)
+	{
+		return (-num);
+	}
+
+	else
+		return (num);
+}
