2a1f8e1489ec8290e0a5c99b6e2705998a07c764
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-06 19:12:40 +0000

    Function Nested Loops Project Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - is a function that adds two integers and returns the result
+ *@num1: store the first number to add
+ *@num2: store the second number to add
+ *Return: return the add result
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
