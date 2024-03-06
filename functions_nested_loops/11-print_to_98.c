e663b9ebbaec994d596b6160545592cd158868ec
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-06 19:35:58 +0000

    Function Nested Loops Project Task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_to_98 - print all numbers from n to 98
+ *
+ *@n: is the variable who store the starting number
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = 0; n < 98; n++)
+		{
+			printf("%d", n);
+		}
+	}
+	else
+	{
+		for (n = 0; n > 98; n--)
+		{
+			printf("%d", n);
+		}
+	}
+printf("98'\n'");
+}
