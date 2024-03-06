2ad0f273f6f1793a64bbd2c7bcd8be6be07e4ada
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-06 14:34:22 +0000

    Function Nested Loops Project Task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_islower - check if a character is lowercase
+ * Return: 1 if is lowercase and 0 if is uppercase
+ *@c: is where the given char is store
+ */
+
+int _islower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
