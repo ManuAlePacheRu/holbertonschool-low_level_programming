94321273e92757fdef19c6b62e3def0ba51e1277
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-06 17:25:06 +0000

    Function Nested Loops Project Task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int min;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
