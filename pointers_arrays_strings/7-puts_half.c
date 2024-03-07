7336cb11c70d577aa089f0d8e0851024f1796fb3
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-07 17:10:39 +0000

    Pointers Arrays and Strings Project Task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts_half - print the half of a string
+ *@str: is a pointer to a string
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	a = str[len] + 1 / 2;
+
+	while (str[a])
+	{
+		_putchar(str[a]);
+		a++;
+	}
+
+	_putchar('\n');
+}
