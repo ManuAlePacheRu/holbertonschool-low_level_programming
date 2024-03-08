9b152f963f21691167ace0beaff730a7e3b5f888
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-08 14:43:05 +0000

    Pointers, Arrays and Strings Project Task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,12 +4,20 @@
  *print_array - prnt an array of char
  *@*a: is a pointer to an array
  *@n: is the number of elements to be printed
+ *@a: is the pointer used in the program
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+printf("\n");
 }
