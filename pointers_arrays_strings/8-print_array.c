1eeea9d233007d676dbb268f6ab59559d227a7e7
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-08 13:42:33 +0000

    Pointers, Arrays and Strings Project Task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,7 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
-		{
-			printf("%d", a[i]);
-
-				if (i < n -1)
-				{
-					printf(", ");
-				}
-		}
+	n=n;
+	for (i = 0; a[i] != '\0'; i++)
+		printf("%d, ", a[i]);
 }
