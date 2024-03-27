d67b026f8f2b84ad5bbe1db4d284557641ac3fc9
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-27 13:18:33 +0000

    Linked lsits Task 0

--- singly_linkled_list/0-print_list.c
+++ singly_linkled_list/0-print_list.c
@@ -8,12 +8,13 @@ int cont;
 
 size_t print_list(const list_t *h)
 {
-
+size_t cont;
+int ncont = 0;
 while (h != NULL)
 {
-	_putchar(h);
+	_putchar(h->str[ncont]);
 	cont ++;
-	h = -> *next;
+	h = h->next;
 }
 
 return (cont);
