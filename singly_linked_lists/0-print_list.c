24866411dd4ea7955a394933b17d50ff246a0fff
ManuAlePacheRu <manualepacheru@gmail.com>
2024-03-27 13:54:55 +0000

    Linked lsits Task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,9 +10,17 @@ size_t print_list(const list_t *h)
 {
 size_t cont;
 int ncont = 0;
+
 while (h != NULL)
 {
+	if (h->str[ncont] == '\0')
+{
+        _putchar('\n');
+}
+	for (ncont = 0; ncont != '\0'; ncont++)
 	_putchar(h->str[ncont]);
+	_putchar('\n');
+
 	cont ++;
 	h = h->next;
 }
