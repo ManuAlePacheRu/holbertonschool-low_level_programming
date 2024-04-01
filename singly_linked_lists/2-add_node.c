1a05aa1b35745c7342168d000b3068fe8b3f97bd
ManuAlePacheRu <manualepacheru@gmail.com>
2024-04-01 15:03:33 +0000

    Linked List Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,19 +1,26 @@
 #include "lists.h"
 
 /**
- *
+ * add_node - add a new node to a existen list
+ *@str: is a const char string in the list
+ *@head: is doble pointer to a list called list_t
+ *Return: Retrun the pointer p to the list p
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *p;
+
+	list_t *p;
+
 	p = malloc(sizeof(list_t));
 
 	p->next = *head;
-	
+
 		*head = p;
 
 	p->str = strdup(str);
+
+	p->len = strlen((*head)->str);
 
 return (p);
 }
