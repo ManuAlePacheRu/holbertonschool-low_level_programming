382f5debdedbd6cbacc54022e8aa2d8409cad80e
ManuAlePacheRu <manualepacheru@gmail.com>
2024-04-01 16:23:16 +0000

    Linked List Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,13 +1,39 @@
 #include "lists.h"
 
 /**
+ * _strlen - take string drection
+ * @s: take the string
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+
+{
+
+	int i = 0;
+
+
+	while (s[i] != '\0')
+	{
+
+		i++;
+
+	}
+
+	return (i);
+
+}
+
+/**
  * add_node - add a new node to a existen list
  *@str: is a const char string in the list
  *@head: is doble pointer to a list called list_t
- *Return: Retrun the pointer p to the list p
+ *Retur: Retrun the pointer p to the list p
  */
 
 list_t *add_node(list_t **head, const char *str)
+
 {
 
 	list_t *p;
