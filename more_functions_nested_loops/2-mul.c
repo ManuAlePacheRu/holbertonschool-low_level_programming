f18eb0ed31011d1f36e2e79951d7100109a87c56
ManuAlePacheRu <manualepacheru@gmail.com>
2024-04-09 13:32:55 +0000

    More Nested Loops Task 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- *
+ * mul - multiply two integers
+ *@a: first number
+ *@b: second number
+ *Return: res
  */
 
 int mul(int a, int b)
 {
 	int res;
+
 	res = a * b;
 
 	return (res);
